df77da5d85792f386517e4e558cdf50e5359102c
Rachelle Lee <rachedeveloper@gmail.com>
2021-07-19 20:24:57 -0700

    Initial Commit

---
+++ RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSApp.swift
+//  RPS
+//
+//  Created by RachelleLee on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
